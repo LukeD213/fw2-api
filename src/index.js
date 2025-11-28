b1c1653bce1c8e63f62f7408305338833fed6bd8
LukeD213 <150153401+LukeD213@users.noreply.github.com>
2025-11-28 09:05:37 -0300

    refactory code 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
  
 app.set('view engine','ejs')
-app.set('views',"../views")
+app.set('views',"./views")
 
 require("../db/db")(app);
 require("../rotas/home")(app);
